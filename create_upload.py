ca51b1a0ede961974ca41d95ebf8eeaabc2d1908
MrJeremyHobbs <mr.hobbsy@gmail.com>
2019-06-07 10:42:26 -0700

    updated to latest version

--- create_upload.py
+++ create_upload.py
@@ -8,9 +8,11 @@ from time import gmtime, strftime
 from re import sub
 from decimal import Decimal
 
+#/ Configurations
+d_path = 'PATH_TO_FTP_SERVER'
+save_path = 'YOUR_SAVE_PATH'
+
 #/Get XML invoice
-d_path = 'PATH_TO_XML_FILE'
-save_path = 'SAVE_PATH'
 invoice_file = easygui.fileopenbox(msg="Select File", title="Select File",default=d_path+"*.xml", multiple=False)
 if invoice_file == None:
     sys.exit()
@@ -29,29 +31,29 @@ doc = untangle.parse(invoice_file)
 xml = doc.payment_data.invoice_list.invoice
 for inv in xml:
     #/Invoice ID
-    Invoice_ID = inv.invoice_number.cdata
+    Invoice_ID               = inv.invoice_number.cdata
     if "UT_RT" in Invoice_ID: continue #/Skips Use Tax invoices
     
     #/Other values
-    Invoice_Dt = inv.invoice_date.cdata
+    Invoice_Dt               = inv.invoice_date.cdata
     vendor_FinancialSys_Code = inv.vendor_FinancialSys_Code.cdata
-    Vendor_Id = vendor_FinancialSys_Code[:10]
-    dt = inv.invoice_ownered_entity.creationDate.cdata
-    Acctg_Dt = dt[4:6]+"/"+dt[6:8]+"/"+dt[0:4]
-    Vchr_Line = '1'   #/Static value
-    GL_Unit = 'SLCMP' #/Static value
-    Descr = 'REGULAR' #/Static value
+    Vendor_Id                = vendor_FinancialSys_Code[:10]
+    dt                       = inv.invoice_ownered_entity.creationDate.cdata
+    Acctg_Dt                 = dt[4:6]+"/"+dt[6:8]+"/"+dt[0:4]
+    Vchr_Line                = '1'       #/Static value
+    GL_Unit                  = 'SLCMP'   #/Static value
+    Descr                    = 'REGULAR' #/Static value
     
     #/Gross Amount
-    Gross_Amt = inv.invoice_amount.sum.cdata
-    Gross_Amt = Decimal(sub(r'[^-\d.]', '', Gross_Amt)) #/includes credit amounts
+    Gross_Amt                = inv.invoice_amount.sum.cdata
+    Gross_Amt                = Decimal(sub(r'[^-\d.]', '', Gross_Amt)) #/includes credit amounts
     
-    #/Skip invoices over $2,500 ()
+    #/Skip invoices over $2,500
     if Gross_Amt > 2500: continue
     
     #/Sales Tax
-    Sales_Tax = inv.vat_info.vat_amount.cdata
-    Sales_Tax = Decimal(sub(r'[^-\d.]', '', Sales_Tax)) #/includes credit amounts
+    Sales_Tax                = inv.vat_info.vat_amount.cdata
+    Sales_Tax                = Decimal(sub(r'[^-\d.]', '', Sales_Tax)) #/includes credit amounts
     
     #/SUT - Must be 'S', 'E', or 'U'
     if Sales_Tax != "": SUT = 'S'
@@ -63,23 +65,23 @@ for inv in xml:
     if Sales_Tax != 0.0 or SUT == 'U': Ship_To_Location = '0820'
     
     #/Overhead (if any)
-    overhead = inv.additional_charges.overhead_amount.cdata
+    overhead                 = inv.additional_charges.overhead_amount.cdata
     
     #/Freight Amount
-    Freight_Amt = inv.additional_charges.shipment_amount.cdata
+    Freight_Amt              = inv.additional_charges.shipment_amount.cdata
     
     if Freight_Amt == '0.0':
         Freight_Amt = overhead
-    Freight_Amt = Decimal(sub(r'[^-\d.]', '', Freight_Amt)) #/includes credit amounts
+    Freight_Amt              = Decimal(sub(r'[^-\d.]', '', Freight_Amt)) #/includes credit amounts
     
     #/Merchandise Amount (Net Amount)
-    Merchandise_Amt = Gross_Amt - Freight_Amt - Sales_Tax
+    Merchandise_Amt          = Gross_Amt - Freight_Amt - Sales_Tax
     
     #/Address Sequence
-    Addr_Seq = vendor_FinancialSys_Code[11:]
+    Addr_Seq                 = vendor_FinancialSys_Code[11:]
     
     #/Blank out zero fields
-    if Sales_Tax == 0.0: Sales_Tax = ""
+    if Sales_Tax   == 0.0: Sales_Tax = ""
     if Freight_Amt == 0.0: Freight_Amt = ""
     
     #/External_id #############################################################
@@ -114,5 +116,5 @@ for inv in xml:
     fh.write(f"{invoice_line}\n")
     
 #/Finish
-fh.close
 easygui.msgbox("Done.", title="Finished", )
+fh.close
